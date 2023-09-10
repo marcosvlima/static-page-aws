fee2908643aa70099627c9c74e841965b8dbc8ad
marcosvlima <marcoslima.xv@gmail.com>
2023-09-10 12:50:32 -0300

    create cache behavier

--- infra/main.tf
+++ infra/main.tf
@@ -18,8 +18,24 @@ resource "aws_cloudfront_distribution" "static_website_distribution" {
   }
 
   enabled             = true
-  is_ipv6_enabled     = true
+  is_ipv6_enabled     = false
   default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+    min_ttl = 0
+    default_ttl = 3600
+    max_ttl = 86400
+  }
   
   viewer_certificate {
     cloudfront_default_certificate = true
