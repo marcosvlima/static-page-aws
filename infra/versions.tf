a7c6a5ea3a08317799e20b827c2a6fe7ce2b6b6a
marcosvlima <marcoslima.xv@gmail.com>
2023-09-10 12:22:46 -0300

    wip

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,24 +1,14 @@
 terraform {
+  required_version = ">= 1.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.29"
+      version = ">= 4.9"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = ">= 1.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.0"
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.7"
     }
   }
 }
