0c2ca8f9af12d80f196ebf58f87a3380722b6246
marcosvlima <marcoslima.xv@gmail.com>
2023-09-10 12:10:56 -0300

    wip

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -1,3 +1,15 @@
+provider "aws" {
+  region = "us-east-1" # CloudFront expects ACM resources in us-east-1 region only
+
+  # Make it faster by skipping something
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+
+  # skip_requesting_account_id should be disabled to generate valid ARN in apigatewayv2_api_execution_arn
+  skip_requesting_account_id = false
+}
+
 module "cdn" {
   source = "terraform-aws-modules/cloudfront/aws"
 
